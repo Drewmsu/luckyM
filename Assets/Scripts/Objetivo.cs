8d4f225b134b5f61f30fe5ccd47fb652dfb3367e
Drewmsu <fran.mp07@gmail.com>
2017-09-15 00:11:11 -0500

    Implementa movimiento al Objetivo

--- Assets/Scripts/Objetivo.cs
+++ Assets/Scripts/Objetivo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Objetivo : MonoBehaviour {
 
-	public float velocidad = 6.5f;
+	public float velocidad;
 	Vector3 newPosition;
 
 	// Use this for initialization
@@ -14,20 +14,6 @@ public class Objetivo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Mouse0)) 
-         	{
-				if (Input.GetMouseButtonDown(0)) 
-				{
-					float step = this.velocidad * Time.deltaTime;
-
-					RaycastHit hit;
-					Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-					if (Physics.Raycast(ray, out hit))
-					{
-						newPosition = hit.point;
-						transform.position = newPosition;
-					}
-				}
-         	}
+		transform.Translate(velocidad*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, velocidad*Input.GetAxis("Vertical")*Time.deltaTime);
 	}
 }
